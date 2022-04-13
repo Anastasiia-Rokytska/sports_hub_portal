e04d3b1da6d71ef398c2031f592b3a96be992c5f
emily0621 <89770447+emily0621@users.noreply.github.com>
2022-04-13 11:10:23 +0300

    First commit

---
+++ src/test/java/com/company/sportHubPortal/SportHubPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.sportHubPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportHubPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
