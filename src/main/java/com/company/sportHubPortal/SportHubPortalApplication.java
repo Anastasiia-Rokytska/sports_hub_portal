614dad10a4593641c4a34bc7263a92e2f7bf1c50
emily0621 <89770447+emily0621@users.noreply.github.com>
2022-04-13 11:47:58 +0300

    Add files via upload

---
+++ src/main/java/com/company/sportHubPortal/SportHubPortalApplication.java
@@ -0,0 +1,13 @@
+package com.company.sportHubPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportHubPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportHubPortalApplication.class, args);
+	}
+
+}
