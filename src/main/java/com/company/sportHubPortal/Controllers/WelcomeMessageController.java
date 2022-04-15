7cbea10aecc4c8acf0f6816279cddbeae6cfc096
emilyyy0621 <emiliia.bondarenko.knm.2020@lpnu.ua>
2022-04-15 17:36:59 +0300

    Getting started on frontend login page

--- src/main/java/com/company/sportHubPortal/Controllers/WelcomeMessageController.java
+++ src/main/java/com/company/sportHubPortal/Controllers/WelcomeMessageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeMessageController {
 
-    @GetMapping("/")
+    @GetMapping("/welcome")
     public String welcomeMessage() {
         return "Welcome to SportHub Portal";
     }
---
+++ src/main/java/com/company/sportHubPortal/Security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.company.sportHubPortal.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin().disable();
+    }
+}
