6fbf5710dbe56d9cd2609bb60766dc0ad0216ec0
Anastasiia-Rokytska <55663939+Anastasiia-Rokytska@users.noreply.github.com>
2022-05-17 13:44:26 +0300

    add emailconfigs file (#22)
    * add emailconfigs file
    
    * add some directories to gitignore
    
    * resolve conflicts

--- src/main/java/com/company/sportHubPortal/Configs/EmailSenderConfiguration.java
+++ src/main/java/com/company/sportHubPortal/Configs/EmailSenderConfiguration.java
@@ -1,23 +1,53 @@
 package com.company.sportHubPortal.Configs;
 
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
 public class EmailSenderConfiguration {
+    private final String EMAIL_CONFIG_PATH = "src/main/resources/configs/emailConfigs.json";
+    private final Logger logger = LoggerFactory.getLogger(EmailSenderConfiguration.class);
     @Bean
     public JavaMailSenderImpl configureJavaMailSender(){
+
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setUsername("sports.hub.portal.0@gmail.com");
-        javaMailSender.setPassword("tsaeptlbupenfiah");
-        javaMailSender.setHost("smtp.gmail.com");
-        javaMailSender.setPort(587);
-        Properties props = javaMailSender.getJavaMailProperties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader(EMAIL_CONFIG_PATH));
+            JSONObject jsonObject = (JSONObject) obj;
+            String username = (String) jsonObject.get("username");
+            String password = (String) jsonObject.get("password");
+            String host = (String) jsonObject.get("host");
+            int port = ((Long)jsonObject.get("port")).intValue();
+
+            javaMailSender.setUsername(username);
+            javaMailSender.setPassword(password);
+            javaMailSender.setHost(host);
+            javaMailSender.setPort(port);
+
+            Properties props = javaMailSender.getJavaMailProperties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            logger.info("JavaMailSender is successfully configured");
+        } catch (IOException e) {
+            logger.info(e.getMessage());
+            e.printStackTrace();
+        } catch (ParseException e) {
+            logger.info(e.getMessage());
+            e.printStackTrace();
+        }
+
+
         return javaMailSender;
     }
 }
